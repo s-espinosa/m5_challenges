93ec428aed920683ada9d3ff57744cea3a8c2752
Sal Espinosa <sespinos@gmail.com>
2021-10-29 14:33:40 -0500

    Turn runner into a spec file

--- tower_of_hanoi/spec/tower_spec.rb
+++ tower_of_hanoi/spec/tower_spec.rb
@@ -1,18 +1,55 @@
+require 'rspec'
 require './lib/tower'
 
-mild = {"A" => [1], "B" => [], "C" => []}
-t = Tower.new(mild)
-t.move!
-puts t.posts
+describe Tower do
+  it 'can move a single disc to a target' do
+    mild = {"A" => [1], "B" => [], "C" => []}
+    t = Tower.new(mild)
+    t.move!
 
-medium = {"A" => [2, 1], "B" => [], "C" => []}
-t = Tower.new(medium)
-t.move!
-puts t.posts
+    expect(t.posts).to eq({"A" => [], "B" => [], "C" => [1]})
+  end
 
-spicy = {"A" => [3, 2, 1], "B" => [], "C" => []}
-t = Tower.new(spicy)
-t.move!
-puts t.posts
+
+  it 'can move two discs to a target' do
+    medium = {"A" => [2, 1], "B" => [], "C" => []}
+    t = Tower.new(medium)
+    t.move!
+
+    expect(t.posts).to eq({"A" => [], "B" => [], "C" => [2, 1]})
+  end
+
+  it 'can move three discs to a target' do
+    spicy = {"A" => [3, 2, 1], "B" => [], "C" => []}
+    t = Tower.new(spicy)
+    t.move!
+
+    expect(t.posts).to eq({"A" => [], "B" => [], "C" => [3, 2, 1]})
+  end
+
+  it 'can move four discs to a target' do
+    start = {"A" => [4, 3, 2, 1], "B" => [], "C" => []}
+    t = Tower.new(start)
+    t.move!
+
+    expect(t.posts).to eq({"A" => [], "B" => [], "C" => [4, 3, 2, 1]})
+  end
+
+  it 'can move ten discs to a target' do
+    start = {"A" => [10, 9, 8, 7, 6, 5, 4, 3, 2, 1], "B" => [], "C" => []}
+    t = Tower.new(start)
+    t.move!
+
+    expect(t.posts).to eq({"A" => [], "B" => [], "C" => [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]})
+  end
+
+  it 'can create a goal for itself' do
+    input = [4, 2, 1]
+    output = [2, 1]
+    tower = Tower.new
+
+    expect(tower.identify_goal(input)).to eq(output)
+  end
+end
 
 
